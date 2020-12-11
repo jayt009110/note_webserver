9d5e52832b37692bce4900690474e94196ac87ea
jayt009110 <1741712780@qq.com>
2020-12-11 17:48:35 +0800

    笔记

---
+++ WebServer/V4/src/main/java/com/webserver/core/WebServer.java
@@ -0,0 +1,46 @@
+package com.webserver.core;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * 小鸟WebServer
+ * 模拟TomCat的基础功能，实现一个简易版的Web容器
+ * 基于TCP协议作为通讯协议，使用HTTP协议与客户端进行交互，完成一系列网络操作
+ */
+public class WebServer {
+    private ServerSocket server;
+
+    public WebServer(){
+
+            try {
+                System.out.println("正在启动服务端");
+                server = new ServerSocket(8088);
+                System.out.println("服务端启动完毕");
+            } catch (IOException e) {
+                e.printStackTrace();
+
+        }
+
+    }
+    public void start(){
+        try {
+            System.out.println("等待客户端连接");
+            Socket socket = server.accept();
+            System.out.println("一个客户端连接了");
+            //启动一个线程，处理该客户端交互
+            ClientHandler handler = new ClientHandler(socket);
+            Thread t = new Thread(handler);
+            t.start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+        WebServer webserver = new WebServer();
+        webserver.start();
+    }
+}
